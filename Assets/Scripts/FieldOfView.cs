5e21db9334c3eaa97c285931e07d5231f727ea72
Christoph Wedenig <christoph@wedenig.org>
2019-12-20 22:09:38 +0100

    FoV working :)
    dis is noice

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+
+    void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+    IEnumerator FindTargetsWithDelay(float delaySec)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delaySec);
+            FindVisibleTargets();
+        }
+    }
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        foreach (var targetCollider in targetsInViewRadius)
+        {
+            var target = targetCollider.transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                //
+                float distance = Vector3.Distance(transform.position, target.position);
+                if(!Physics.Raycast(transform.position, dirToTarget, distance, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        // We swap Cos and Sin because in Unity the unit circle starts on top and goes clockwise
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
