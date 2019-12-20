ebeba4d0f22ee079c619d2809b40a9b425c43de0
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2019-12-20 22:54:02 +0100

    Add LWRP

---
+++ Assets/Scripts/RacconRotateScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacconRotateScript : MonoBehaviour
+{
+    public float RotationSpeed = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.one * Time.deltaTime * RotationSpeed);
+
+        transform.parent.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * 5f, 0, Input.GetAxis("Vertical") * Time.deltaTime * 5f);
+    }
+}
