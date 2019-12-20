71b7a21e8a7f9a42afdace89bfe118fc0741ac99
Alex <allercher@edu.aau.at>
2019-12-20 23:30:19 +0100

    hahahaha

--- Assets/Scripts/Movement/Player.cs
+++ Assets/Scripts/Movement/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
@@ -12,7 +13,13 @@ public class Player : MonoBehaviour
     private float moveSpeed = 6;
     private Vector3 _velocity;
 
+    public UnityEvent HandsOverMovement; 
 
+    /// <summary>
+    /// Boolean if the movement is active for this raccoon.
+    /// </summary>
+    public bool IsMovementActive { get; set; }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +31,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         _velocity = new Vector3(_inputHandler.GetLeftHorizontalValue(),0, _inputHandler.GetLeftVerticalValue()).normalized * moveSpeed;
+        
+        // movement
+        if (!IsMovementActive)
+            return;
+
+        if (_inputHandler.IsChangeFeaturePressed())
+        {
+            Debug.Log("Invoke");
+            HandsOverMovement?.Invoke();
+        }
     }
     
     private void FixedUpdate()
     {
+        if (!IsMovementActive)
+            return;
+        
         _rigidbody.MovePosition(_rigidbody.position + _velocity * Time.fixedDeltaTime);
     }
 }
