69a05ad6f42db32891e156931ca4ba5fcc35913a
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2019-12-21 14:55:24 +0100

    Look at camera script

---
+++ Assets/LookAtCameraScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LookAtCameraScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var position = Camera.main.transform;
+        transform.LookAt(position, Vector3.up);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+    }
+}
