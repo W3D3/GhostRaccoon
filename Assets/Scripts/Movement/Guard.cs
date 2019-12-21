877c7b822685eeca15137027c31f82e7e4f15027
Christoph Wedenig <christoph@wedenig.org>
2019-12-21 14:04:44 +0100

    Added guard waypoints

---
+++ Assets/Scripts/Movement/Guard.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Guard : MonoBehaviour
+{
+    public List<Vector3> waypoints;
+    private int index;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        transform.position = waypoints[0];
+        transform.LookAt(waypoints[1 % waypoints.Count]);
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        foreach (var waypoint in waypoints)
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(waypoint, 0.3f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(agent);
+        if (agent.remainingDistance == 0f)
+        {
+            Debug.DrawLine(transform.position, waypoints[index % waypoints.Count]);
+            if (waypoints != null)
+            {
+                agent.SetDestination(waypoints[index % waypoints.Count]);
+                index++;
+            }
+        }
+    }
+
+    void alert(Transform emitterPos)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Movement/SoundEmitter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Script for GameObjects with a transform
+ * call makeSound to emit a sound and alert all guards in the specified radius
+ */
+public class SoundEmitter : MonoBehaviour
+{
+    public LayerMask guardMask;
+
+    public float soundRadius;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void makeSound()
+    {
+        Collider[] targetsInSoundRadius = Physics.OverlapSphere(transform.position, soundRadius, guardMask);
+
+        foreach (var target in targetsInSoundRadius)
+        {
+            var guard = target.GetComponent<Guard>();
+        }
+    }
+}
