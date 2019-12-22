4ccbec4447f4b17892d91c3049bf556aab9a81d2
Alex <allercher@edu.aau.at>
2019-12-22 13:02:04 +0100

    Tutorial + trigger scripts

---
+++ Assets/Scripts/ShowObjectFromTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using TMPro;
+using UnityEngine;
+
+public class ShowObjectFromTrigger : MonoBehaviour
+{
+    public GameObject TextToActivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Raccoon"))
+            TextToActivate.SetActive(true);
+    }
+}
